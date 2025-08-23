ac8562d09869d1e3d460f6181caf30867642d53c
leomeurer <lmeurer@gmail.com>
2025-08-23 15:09:08 -0300

    Adiciona tratamento para iniciar o video em um tempo aleatório

--- shortGPT/editing_utils/handle_videos.py
+++ shortGPT/editing_utils/handle_videos.py
@@ -24,21 +24,25 @@ def getYoutubeVideoLink(url):
     except Exception as e:
         raise Exception(f"Failed getting video link from the following video/url {url} {e.args[0]}")
 
-def extract_random_clip_from_video(video_url, video_duration, clip_duration, output_file):
+def extract_random_clip_from_video(video_url, video_duration, clip_duration, output_file, start_time=None):
     """Extracts a clip from a video using a signed URL.
     Args:
         video_url (str): The signed URL of the video.
-        video_url (int): Duration of the video.
-        start_time (int): The start time of the clip in seconds.
+        video_duration (int): Duration of the video.
         clip_duration (int): The duration of the clip in seconds.
         output_file (str): The output file path for the extracted clip.
+        start_time (int, optional): The start time of the clip in seconds. If not provided, a random start time is selected.
     """
     if not video_duration:
         raise Exception("Could not get video duration")
     if not video_duration*0.7 > 120:
         raise Exception("Video too short")
-    start_time = video_duration*0.15 + random.random()* (0.7*video_duration-clip_duration)
     
+    # Use provided start_time or generate a random one
+    if start_time is None:
+        start_time = video_duration*0.15 + random.random()* (0.7*video_duration-clip_duration)
+   
+ 
     command = [
         'ffmpeg',
         '-loglevel', 'error',
